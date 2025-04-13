cd212d9b8929afa83029dd2ca4f0d4a8ecdaac92
KangMinJun <127291567+MinJunKKang@users.noreply.github.com>
2025-04-13 18:30:56 +0900

    [Unrated] Title: 정ㅋ벅ㅋ, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Unrated/1237. 정ㅋ벅ㅋ/정ㅋ벅ㅋ.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cout << "문제의 정답";
+
+    return 0;
+}
