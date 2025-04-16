27ae12d186608f2ceab6313eb15568f93e49a799
KangMinJun <127291567+MinJunKKang@users.noreply.github.com>
2025-04-16 14:52:56 +0900

    [Bronze V] Title: 徒競走 (Footrace), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/33165. 徒競走 （Footrace）/徒競走 （Footrace）.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int A, B;
+    cin >> A;
+    cin >> B;
+
+    cout << A * B;
+
+    return 0;
+}
