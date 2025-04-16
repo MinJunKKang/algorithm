0ae025c959c2004ae880883e432b5a064f1c81c3
KangMinJun <127291567+MinJunKKang@users.noreply.github.com>
2025-04-16 14:54:52 +0900

    [Bronze V] Title: Micromasters, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/33178. Micromasters/Micromasters.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    cout << (n * 10) / 100;
+
+    return 0;
+}
