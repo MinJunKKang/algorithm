8096f1b042ffd9609b42ec9c3ef5a97e8ac5662d
KangMinJun <127291567+MinJunKKang@users.noreply.github.com>
2025-06-23 13:48:50 +0900

    [Gold II] Title: 꼬인 전깃줄, Time: 8 ms, Memory: 2924 KB -BaekjoonHub

--- 백준/Gold/1365. 꼬인 전깃줄/꼬인 전깃줄.cc
+++ 백준/Gold/1365. 꼬인 전깃줄/꼬인 전깃줄.cc
@@ -8,7 +8,7 @@ int binary_search(vector<int> &dp, int target) {
     int ans = rgt + 1;
 
     while (lft <= rgt) {
-        int mid = (lft + rgt) / 2;
+        int mid = lft + (rgt - lft) / 2;
         if (dp[mid] >= target) {
             ans = mid;
             rgt = mid - 1;
@@ -21,21 +21,22 @@ int binary_search(vector<int> &dp, int target) {
 }
 
 int main() {
-    int n;
-    cin >> n;
+    ios_base::sync_with_stdio(false);
+    cout.tie(NULL);
+    cin.tie(NULL);
 
-    vector<int> v(n);
-    for (int i = 0; i < n; i++) cin >> v[i];
-
+    int N; cin >> N;
     vector<int> dp;
 
-    for (int i = 0; i < n; i++) {
-        int pos = binary_search(dp, v[i]);
-        if (pos == dp.size()) dp.push_back(v[i]);
-        else dp[pos] = v[i];
+    vector<int> line(N);
+    for (int i = 0; i < N; i++) cin >> line[i];
+
+    for (int i = 0; i < N; i++) {
+        int pos = binary_search(dp, line[i]);
+        if (pos == dp.size()) dp.push_back(line[i]);
+        else dp[pos] = line[i];
     }
 
-    cout << n - dp.size();
-
+    cout << N - dp.size();
     return 0;
 }
