14cf8ea0a12c2a3e354ae6365b62809150560f4b
KangMinJun <127291567+MinJunKKang@users.noreply.github.com>
2025-06-23 14:42:04 +0900

    [Gold II] Title: 주식, Time: 24 ms, Memory: 2304 KB -BaekjoonHub

---
+++ 백준/Gold/12014. 주식/주식.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int binary_search(vector<int> &dp, int target) {
+    int lft = 0, rgt = dp.size() - 1;
+    int ans = rgt + 1;
+
+    while (lft <= rgt) {
+        int mid = lft + (rgt - lft) / 2;
+        if (dp[mid] >= target) {
+            ans = mid;
+            rgt = mid - 1;
+        } else {
+            lft = mid + 1;
+        }
+    }
+
+    return ans;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cout.tie(NULL);
+    cin.tie(NULL);
+
+    int T; cin >> T;
+
+    int cnt = 1;
+    while (T--) {
+        int N, K; cin >> N >> K;
+        vector<int> dp;
+
+        vector<int> line(N);
+        for (int i = 0; i < N; i++) cin >> line[i];
+
+        for (int i = 0; i < N; i++) {
+            int pos = binary_search(dp, line[i]);
+            if (pos == dp.size()) dp.push_back(line[i]);
+            else dp[pos] = line[i];
+        }
+
+        cout << "Case #" << cnt << '\n';
+        if (dp.size() >= K) {
+            cout << 1;
+        } else cout << 0;
+        cout << '\n';
+        
+        cnt++;
+    }
+
+    return 0;
+}
